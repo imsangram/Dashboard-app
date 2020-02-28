63ed1d69d6edc0211d877beba32ac51fc864059c
Sangram Nandkhile <sangram.nandkhile@gmail.com>
2020-02-29 01:56:39 +1100

    Added swagger documentation and enabled cors

--- middleware/routeSetup.js
+++ middleware/routeSetup.js
@@ -1,11 +1,20 @@
 const userRouter = require('../modules/users/user.routes'),
     authorise = require('./authorise'),
-    loginRouter = require('../modules/login/login.routes');
+    loginRouter = require('../modules/login/login.routes'),
+    swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('../swagger.json'),
+    cors = require('cors');
 
 module.exports = app => {
+
+    //enable cors
+    app.use(cors());
     app.get('/', (req, res) => {
         res.send('Hello world !');
     });
+
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     // Login router
     app.use('/api/login', loginRouter);
